fd982fa366688d329bafd0b2674a537d8ebdfcec
Yoan Agostini <yoan.agostini@gmail.com>
2013-05-09 19:00:34 +0200

    Fix typo

--- pytwidl.py
+++ pytwidl.py
@@ -1,7 +1,7 @@
-#!/usr/env python
+#!/usr/bin/env python
 
 """
-Copyright Yoan Agostini <yoan.agostiniATgmailDOTcom>
+Copyright Yoan Agostini <yoanDOTagostiniATgmailDOTcom>
 
 PyTwiDL is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
